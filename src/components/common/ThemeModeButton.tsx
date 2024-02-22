4728b0148006cd54500e7c64cdf375fd58586d9e
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-02-22 14:10:16 +0200

    fix: icon button having a full screen click animation

--- src/components/common/ThemeModeButton.tsx
+++ src/components/common/ThemeModeButton.tsx
@@ -1,25 +1,26 @@
 import { useContext } from "react";
 import { ThemeModeContext } from "../../context/ThemeModeContext";
-import { IconButton, SxProps } from "@mui/material";
+import { Box, IconButton, SxProps } from "@mui/material";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import NightlightIcon from "@mui/icons-material/Nightlight";
 
 type ThemeModeButtonProps = {
   currentMode: "light" | "dark";
-  sx?: SxProps
+  sx?: SxProps;
 };
-export const ThemeModeButton = ({ currentMode,sx}: ThemeModeButtonProps) => {
+export const ThemeModeButton = ({ currentMode, sx }: ThemeModeButtonProps) => {
   const { toggleThemeMode } = useContext(ThemeModeContext);
 
   return (
-    <IconButton
-      style={{ lineHeight: "0px" }}
-      size="large"
-      onClick={() => toggleThemeMode()}
-      aria-label="Toggle Theme"
-      sx={sx}
-    >
-      {currentMode === "light" ? <NightlightIcon /> : <LightModeIcon />}
-    </IconButton>
+    <Box sx={sx}>
+      <IconButton
+        style={{ lineHeight: "0px" }}
+        size="large"
+        onClick={() => toggleThemeMode()}
+        aria-label="Toggle Theme"
+      >
+        {currentMode === "light" ? <NightlightIcon /> : <LightModeIcon />}
+      </IconButton>
+    </Box>
   );
 };
